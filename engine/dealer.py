423757bcf50848c959e1f9e92458c5501d776706
ycolinta <ycolinta@uvm.edu>
2026-02-25 13:41:45 -0500

    Added working dealer class

---
+++ engine/dealer.py
@@ -0,0 +1,68 @@
+# Dealer "builds" the 106 tiles, randomly shuffles
+# Deals 14 to each player,
+# except a single player whom Dealer gives 15
+# Puts back the remaining tiles in the draw_pile (middle pile)
+
+import random
+
+from engine import draw_pile
+from engine.tile import Tile
+from engine.player import Player
+from engine.draw_pile import DrawPile
+from engine.board import Board
+
+class Dealer:
+    def __init__(self):
+        self.rng = random.Random()
+
+    def build_okey_set(self):
+        # 106 total tiles
+        # Numbers 1-13; 2 copies of each
+        # 2 jokers
+        # 4 colors
+
+        tiles = []
+        for color in Tile.colors:
+            for number in range(1, 14):
+                # appending two copies of each tile
+                tiles.append(Tile(color, number, False, 0))
+                tiles.append(Tile(color, number, False, 1))
+
+        # Adding the jokers
+        tiles.append(Tile(None, None, True, 0)) # joker holding value to be implemented later
+        tiles.append(Tile(None, None, True, 1))
+
+        return tiles
+
+    # Dealer to set up a new round,
+    def deal_new_round(self, player_names, starting_player_idx = 0):
+        if len(player_names) != 4:
+            raise AttributeError('Okey game requires four players.')
+
+        # creating player objs
+        players = []
+        for name in player_names:
+            players.append(Player(name))
+
+        # Build + shuffle
+        tiles = self.build_okey_set()
+        self.rng.shuffle(tiles)
+
+        # Deal 14 tiles to each player
+        for i in range(14):
+            for player in players:
+                player.draw_tile(tiles.pop())
+
+        # Starting player is dealt 15 tiles
+        players[starting_player_idx].draw_tile(tiles.pop())
+
+        # return board state for the round
+        return Board(players, draw_pile, starting_player_idx)
+
+
+
+
+
+
+
+
