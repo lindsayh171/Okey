f46751c845f5ba7202f61e70b334530e3afb3cb0
ycolinta <ycolinta@uvm.edu>
2026-02-24 22:51:09 -0500

    Add minimal Tile class for engine

---
+++ engine/Tile.py
@@ -0,0 +1,16 @@
+# engine/Tile.py
+
+COLORS = ("red", "blue", "black", "orange")
+
+class Tile:
+    def __init__(self, color, number, is_joker=False, copy_id = 0):
+        self.color = color
+        self.number = number
+        self.is_joker = is_joker
+        self.copy_id = copy_id  # two copies of each # tile - this distinguishes duplicate tiles
+
+
+    def __repr__(self):
+        if self.is_joker:
+            return "JOKER"
+        return f"{self.color}-{self.number}({self.copy_id})" # for printing clearly
