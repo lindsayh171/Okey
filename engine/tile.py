03ceb65c518c50d94b718a6f0ec09ce857b1d0fe
Zachary <zbelkin@uvm.edu>
2026-02-25 19:49:46 -0500

    Added suits

--- engine/tile.py
+++ engine/tile.py
@@ -2,13 +2,16 @@
 
 class Tile:
     colors = ("red", "blue", "black", "orange")
-    def __init__(self, color, number, is_joker=False, copy_id = 0):
+    suits = ("circle, triangle, square, star")
+    def __init__(self, color, number, suit, is_joker=False, copy_id = 0):
         self.color = color
-        self.number = number
+        self.number = number #Numbers are 1 to 13
+        self.suit = suit
         self.is_joker = is_joker
         self.copy_id = copy_id  # two copies of each # tile - this distinguishes duplicate tiles
 
     def __repr__(self):
         if self.is_joker:
             return "JOKER"
-        return f"{self.color}-{self.number}({self.copy_id}) of 2 copy" # for printing clearly
+        # for printing clearly
+        return f"{self.color}-{self.suits}-{self.number}({self.copy_id}) of 2 copy" 
